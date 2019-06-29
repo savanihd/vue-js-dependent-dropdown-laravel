08e14ed8c5187c876c364c559bcaa28667435669
Hardik Savani <itsolutionstuff@gmail.com>
2019-06-29 15:27:22 +0530

    update readme file

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -8,17 +8,27 @@ use App\State;
 
 class APIController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function getCountries()
     {
-    	$data = Country::get();
+        $data = Country::get();
 
-    	return response()->json($data);
+        return response()->json($data);
     }
 
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function getStates(Request $request)
     {
-    	$data = State::where('country_id', $request->country_id)->get();
+        $data = State::where('country_id', $request->country_id)->get();
 
-    	return response()->json($data);
+        return response()->json($data);
     }
 }
